c21b46c099b6ce822c6f7dff36de546ae2777c0d
Fathan <fathan.naufal@ui.ac.id>
2024-04-17 21:00:26 +0700

    add: sell feature home page

---
+++ src/pages/sell/SellHomePage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SellHomePage = () => {
+    return (
+        <div>
+            <h1>Sell your Item here!</h1>
+        </div>
+    );
+};
+
+export default SellHomePage;
