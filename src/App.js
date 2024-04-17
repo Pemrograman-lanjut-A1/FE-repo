06cd1f283ea6cdc11ba4482f4234a715cd0324dd
Faris Zhafir Faza <111985990+Marsupilamieue@users.noreply.github.com>
2024-04-17 10:42:16 +0700

    Add Top Up Page skeleton

--- src/App.js
+++ src/App.js
@@ -12,6 +12,8 @@ import StaffHomePage from "./pages/staff-dashboard/staff-homepage";
 import ViewAnnouncements from './pages/staff-dashboard/view-announcements';
 import ViewWaitingPayments from './pages/staff-dashboard/view-waiting-payments';
 import ViewWaitingTopUps from './pages/staff-dashboard/view-waiting-top-ups';
+import TopUpHomePage from './pages/payment/topup/TopUpHomePage';
+import CreateTopUpPage from './pages/payment/topup/CreateTopUpPage';
 
 
 function App() {
@@ -29,6 +31,8 @@ function App() {
               path="/staff/view-waiting-top-ups"
               element={<ViewWaitingTopUps />}
           />
+          <Route path="/topup/" element={<TopUpHomePage/>} />
+          <Route path="/topup/create" element={<CreateTopUpPage/>}/>
       </Routes>
     </Router>
 );
---
+++ src/pages/payment/topup/CreateTopUpPage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+ 
+const CreateTopUpPage = () => {
+    return (
+        <div>
+            <h1>Create Top Up Page</h1>
+        </div>
+    );
+};
+ 
+export default CreateTopUpPage;
