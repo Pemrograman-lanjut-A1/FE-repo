b76879ba4226ed824f9af635dbc6fbd86be9a22b
Faris Zhafir Faza <111985990+Marsupilamieue@users.noreply.github.com>
2024-05-08 20:58:00 +0700

    add async service

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 
 import React from "react";
@@ -12,8 +11,7 @@ import StaffHomePage from "./pages/staff-dashboard/staff-homepage";
 import ViewAnnouncements from './pages/staff-dashboard/view-announcements';
 import ViewWaitingPayments from './pages/staff-dashboard/view-waiting-payments';
 import ViewWaitingTopUps from './pages/staff-dashboard/view-waiting-top-ups';
-import TopUpHomePage from './pages/payment/topup/TopUpHomePage';
-import CreateTopUpPage from './pages/payment/topup/CreateTopUpPage';
+import PaymentPage from './pages/payment/topup/PaymentPage';
 
 
 function App() {
@@ -31,8 +29,7 @@ function App() {
               path="/staff/view-waiting-top-ups"
               element={<ViewWaitingTopUps />}
           />
-          <Route path="/topup/" element={<TopUpHomePage/>} />
-          <Route path="/topup/create" element={<CreateTopUpPage/>}/>
+          <Route path="/payment/" element={<PaymentPage/>} />
       </Routes>
     </Router>
 );
