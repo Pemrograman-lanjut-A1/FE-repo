2d20c9e4b580779bca2b30a03cdd72577abe041d
Faris Zhafir Faza <111985990+Marsupilamieue@users.noreply.github.com>
2024-05-23 22:47:49 +0700

    Add Logout Button

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,21 @@
 import React from "react";
+import AuthService from "./auth/service/AuthService";
+import { useNavigate } from 'react-router-dom';
+import AuthMiddleware from "./auth/service/AuthMiddleware";
  
 const Home = () => {
+    const navigate = useNavigate();
+
+    const handleLogout = async () => {
+        const token = localStorage.getItem('token');
+        await AuthService.logout(token)
+        AuthMiddleware.logout();
+        navigate('/signin');
+    }
     return (
         <div>
             <h1>Home</h1>
+            <button onClick={handleLogout}>Logout</button>
         </div>
     );
 };
