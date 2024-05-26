befd41424aaf72f4724f507d1402f4547d5adfcb
DaWanAnOnli <joel.rompis@gmail.com>
2024-05-26 20:54:08 +0700

    delete unused files & imports

--- src/App.js
+++ src/App.js
@@ -6,9 +6,7 @@ import{
 } from "react-router-dom";
 
 import Home from './pages/Home';
-import StaffHomePage from "./pages/staff-dashboard/staff-homepage";
 import ViewAnnouncementsPage from './pages/staff-dashboard/announcement/view-announcements-page';
-import ViewWaitingPayments from './pages/staff-dashboard/view-waiting-payments';
 
 import ViewWaitingTopUpsPage from './pages/staff-dashboard/confirm-topup/view-waiting-top-ups-page';
 import CreateAnnouncementPage from './pages/staff-dashboard/announcement/create.announcement.page';
@@ -28,12 +26,10 @@ function App() {
           <Route exact path="/" element={<Home />} />
           <Route exact path="/signin" element={<LoginPage />} />
           <Route exact path="/signup/:type" element={<RegisterPage />} />
-          <Route path="/staff/homepage" element={<StaffHomePage />} />
           <Route
               path="/staff/view-announcement"
               element={<ViewAnnouncementsPage />}
           />
-          <Route path="/staff/view-waiting-payments" element={<ViewWaitingPayments />} />
           <Route path="/staff/create-announcement" element={<CreateAnnouncementPage />}/>
           <Route path="staff/view-waiting-top-ups" element={<ViewWaitingTopUpsPage/>}/>
           <Route path="/payment/" element={<PaymentPage/>} />
